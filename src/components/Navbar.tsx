7af0cff9ad88d0a7061cad4eea198f707d452fc9
Quake <142627174+SJ-209@users.noreply.github.com>
2025-05-04 20:16:40 +0100

    blurred header with border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-full top-0 bg-white z-50">
+    <nav className="fixed w-full top-0  z-50 backdrop-blur-sm border-b">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-10">
-        <div className="font-mono font-bold text-xl">DARK HARBOUR</div>
+        <div className="font-mono font-bold text-xl">DARK HARBOUR STUDIOS</div>
         <div className="hidden md:flex space-x-6 text-xs">
           <a href="#about" className="hover:underline">ABOUT</a>
           <a href="#showReel" className="hover:underline">SHOW REEL</a>
